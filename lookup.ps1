63350dd8c0aa92faaabfc5b3fd82692689502fca
Akash Kundu <akashkundu92@gmail.com>
2017-04-03 21:22:16 -0500

    Add files via upload

---
+++ lookup.ps1
@@ -0,0 +1,22 @@
+﻿$SPACE = '+'
+$googleSearch = 'https://www.google.com/#q='
+$youtubeSearch = 'https://www.youtube.com/results?search_query='
+    
+if($($args[0]) -eq "goo")
+{
+    for($i = 1; $i -lt $args.Length; $i++)
+    {
+        $nextParam += $($($args[$i])+$SPACE)
+    }
+    $nextSearch = $($googleSearch + $nextParam)
+    Start-Process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList $nextSearch
+}
+elseif($($args[0]) -eq "yt")
+{
+    for($i = 1; $i -lt $args.Length; $i++)
+    {
+        $nextParam += $($($args[$i])+$SPACE)
+    }
+    $nextSearch = $($youtubeSearch + $nextParam)
+    Start-Process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList $nextSearch
+}
